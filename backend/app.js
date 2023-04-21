9b5a76bdc6bee603eb93de9dfacc4c882c43aa06
TailorBird93 <99030950+TailorBird93@users.noreply.github.com>
2023-04-21 04:44:28 +0100

    Created Goal forms and Items for frontend display

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path= require('path')
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,13 +14,22 @@ app.use(bodyParser.json());
 const photoRoutes = require('./routes/photoRoutes');
 const userRoutes= require('./routes/userRoutes');
 const favoritesRoutes=require('./routes/favoritesRoutes')
+const goalRoutes=require('./routes/goalRoutes')
 
 app.use(express.json());
 app.use('/api/photos', photoRoutes);
 app.use('/api/user', userRoutes);
-app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/goals', goalRoutes);
 app.use('/api/favorites', favoritesRoutes)
 
+// Serve frontend
+
+if(process.env.NODE_ENV==='production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.get('*', (req,res)=> res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else{
+  app.get('/',(req, res)=> res.send('Set to production'))
+}
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
